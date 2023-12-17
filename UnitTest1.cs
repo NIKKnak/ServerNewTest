cc434d220f76ab4c2a702ad8e47fbe2fd4233209
NIKOLAY <113236671+NIKKnak@users.noreply.github.com>
2023-12-17 20:34:50 +0300

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -13,7 +13,7 @@ namespace ServerTest
             // Arrange
             var messageSource = new Mock<IMessageSource>();
             var server = new Server(messageSource.Object);
-            server.Clients.Add("User1", new IPEndPoint(IPAddress.Parse("192.168.1.1"), 1234));
+            server.Clients.Add("User1", new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234));
             var testMessage = new NetMessage { NickNameFrom = "User2", NickNameTo = "User1", Text = "Test Message" };
             server.RelyMessage(testMessage).Wait();
             messageSource.Verify(mock => mock.SendAsync(testMessage, It.IsAny<IPEndPoint>()), Times.Once);
